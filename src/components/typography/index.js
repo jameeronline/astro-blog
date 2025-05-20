26b75b9286f0cf2bceaa341853797125a4fd5343
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2025-05-20 19:39:12 +0300

    features pages added

---
+++ src/components/typography/index.js
@@ -0,0 +1,8 @@
+import BaseHeading from "./BaseHeadings.astro";
+
+export const H1 = (props) => <BaseHeading level={1} {...props} />;
+export const H2 = (props) => <BaseHeading level={2} {...props} />;
+export const H3 = (props) => <BaseHeading level={3} {...props} />;
+export const H4 = (props) => <BaseHeading level={4} {...props} />;
+export const H5 = (props) => <BaseHeading level={5} {...props} />;
+export const H6 = (props) => <BaseHeading level={6} {...props} />;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    // add other paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // other plugins...
+  ],
+};
