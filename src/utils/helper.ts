239c45be49685655da8ee8d5a45fc5e01fdc1071
Jamal Mohamed Ameer <jamalbe2004@gmail.com>
2025-06-11 12:17:50 +0300

    pagination added for index pages

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -18,3 +18,18 @@ export function slugify(text: string): string {
     .replace(/[^\w-]+/g, "") // Remove non-word characters except hyphens
     .replace(/--+/g, "-"); // Replace consecutive hyphens with a single hyphen
 }
+
+//format date
+import { format } from "date-fns";
+export function formatDate(
+  date: Date | string,
+  dateFormat: string = "MMM d, yyyy"
+): string {
+  return format(new Date(date), dateFormat);
+}
+
+//capitalize
+export function capitalize(text: string): string {
+  if (!text) return "";
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
